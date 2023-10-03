32c4f725b421da9113a1337d36d4f56007f2bb7f
ArnauFernandez <arnau.fernandez.7e4@itb.cat>
2023-10-03 10:16:12 +0200

    4685

---
+++ UF1 Progrmació Estructurada/A2 Manipulació de Dades/bitrthdaycelebrated.py
@@ -0,0 +1,12 @@
+"""
+Arnau Fernandez Pinar
+ASIXc M03 2023 UF1
+Javier Amaya
+3/10/2023
+"""
+diaAniversari=int(input("introdueix un dia del mes en números: "))
+mesAniversari=int(input("introdueix en numeros "))
+diaActual=int(input("introdueix un dia del mes en números: "))
+mesActual=int(input("introdueix un dia del mes en números: "))
+if diaAniversari == diaActual and mesAniversari >= mesActual:
+    print("Aniversari Celebrat")
