f8af011bd99abf1ed6a9aebbf303c58ca69b0198
ArnauFernandez <arnau.fernandez.7e4@itb.catt>
2023-10-24 11:54:58 +0200

    rgwaer

---
+++ UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A3-Estructures_De_Selecció/Howmanydaysinmonth.py
@@ -0,0 +1,10 @@
+try:
+    question=input()
+    if question == "1" or question== "3" or question== "5" or question=="7" or question=="8" or question=="10" or question== "12":
+        print(31)
+    elif question == "2":
+        print(28,29)
+    elif question == "6" or question== "90" or question== "11":
+            print(30)
+except:
+    print("error")
