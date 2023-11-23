bebf3aec2e25bd4842822b168e3088ad7cb594a7
ArnauFernandez <arnau.fernandez.7e4@itb.car>
2023-11-23 08:40:05 +0100

    23/11/2023

--- UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A4_Estructrures_De_Repetició/piramid.py
+++ UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A4_Estructrures_De_Repetició/piramid.py
@@ -1,6 +1,5 @@
 CONT=0
 SIMBOLO="🧱"
 cantidad=int(input())
-for base in range(cantidad):
-    CONT = CONT + 1
-    print(SIMBOLO * CONT)
+for base in range(1,cantidad,+1):
+    print(SIMBOLO * base)
