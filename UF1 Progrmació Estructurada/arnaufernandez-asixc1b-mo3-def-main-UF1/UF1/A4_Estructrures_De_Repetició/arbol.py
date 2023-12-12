44f00f1c901463b0d569b729ba1fd6496e2e7c4a
arnaufernandez <arnau.fernandez.7e4@itb.cat>
2023-12-12 12:10:24 +0100

    11/12/2023

--- UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A4_Estructrures_De_Repetició/arbol.py
+++ UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A4_Estructrures_De_Repetició/arbol.py
@@ -1,11 +1,10 @@
 from time import sleep
-ARBOL="🧱"
+ARBOL="🌿"
 TRONCO="🪵"
 mida = int(input())
 for i in range(1, mida+1):
-    print(" "*(mida- i),ARBOL*i)
-    sleep(1)
+
+        print(" "*(mida- i),ARBOL*i)
 for i in range(mida):
         print(" "*(mida-1),TRONCO)
-        sleep(1)
 print(" ")
