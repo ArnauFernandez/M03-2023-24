69f1db673f225298d685e10b9f6a21b67b1f76d2
arnau [itb] fernandez pinar <arnau.fernandez.7e4@itb.cat>
2023-12-13 21:27:23 +0100

    13/12/2023 2

---
+++ UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A4_Estructrures_De_Repetició/e5.py
@@ -0,0 +1,19 @@
+try:
+    vocales='aeiou'
+    consonants='bcdfghjklmnpqrstwxyz'
+    texto='0'
+    caracteresesp="/$%€@|ª="
+    while texto not in vocales or texto not in consonants:
+        texto = input("Ingrese: ")
+        if texto in vocales:
+            print("Vocal")
+        elif texto in consonants:
+            print("Consonant")
+        elif texto ==' ':
+            break
+        elif texto in caracteresesp:
+            print("caracter especial")
+        elif len(texto)>1:
+            print("no valido")
+except:
+    print("caracteres no permitidos")
