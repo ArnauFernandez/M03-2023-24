0e77002fb21931a3e22237a088775d95fed7a5ee
arnaufernandez <arnau.fernandez.7e4@itb.cat>
2024-01-16 11:41:59 +0100

    16/01/2023

---
+++ UF1 Progrmació Estructurada/arnaufernandez-asixc1b-mo3-def-main-UF1/UF1/A5_Vectors_arrays/DNIcalculator.py
@@ -0,0 +1,11 @@
+"""
+Arnau Fernandez Pinar
+M03 UF1 A5
+DNIcalculator.py
+"""
+DNILETTER=("t","r","w","a","g","m","y","f","p","d","x","b","n","j","z","s","q","v","h","l","c","k","e")
+try:
+    dni=int(input(""))
+    print(DNILETTER[dni%23])
+except:
+    print("error")
