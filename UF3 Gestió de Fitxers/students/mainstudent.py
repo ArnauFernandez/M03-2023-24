cafda2428a82885e0a46bf19d6d251f3eb1165ff
sjo <arnau.fernandez.7e4@itb.cat>
2024-04-17 12:29:01 +0200

    17/04/2024

--- UF3 Gestió de Fitxers/students/mainstudent.py
+++ UF3 Gestió de Fitxers/students/mainstudent.py
@@ -1,2 +1,4 @@
 from studentsDS import *
+from studentsDS2 import *
 readfile('student.stats')
+readfile2('student2.stats')
--- UF3 Gestió de Fitxers/students/studentsDS2.py
+++ UF3 Gestió de Fitxers/students/studentsDS2.py
@@ -1,11 +1,9 @@
-import os
-def readfile(path):
+def readfile2(path):
     file=open(path,mode='rt',encoding='utf-8')
-    contenido = [int(nota) for nota in file.read().split()]
+    contenido = [int(nota) for nota in file.read().split() if nota.isdigit()]
     file.close()
     media=sum(contenido)/len(contenido)
     print("nota minima: ", min(contenido))
     print("nota máxima: ", max(contenido))
     print("la media es : ",round(media,3))
     return contenido
-
